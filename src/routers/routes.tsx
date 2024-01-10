66ea03ec78b5f1dde6df7a141811115db5227bd7
Damien Shen <dongmingshen@icloud.com>
2024-01-10 08:59:26 -0800

    OK

--- src/routers/routes.tsx
+++ src/routers/routes.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useLazy } from "../hooks/useLazy";
+// import { useLazy } from "../hooks/useLazy";
 
 function AppRoutes() {
-  // Lazy-loaded components using the useLazy hook
-  const LazyFeed = useLazy(() => import("../screens/Home"));
-  const LazyAbout = useLazy(() => import("../screens/About"));
-  const LazyQuestion = useLazy(() => import("../core/QuestionDetail"));
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LazyFeed />} />
-        <Route path="/about" element={<LazyAbout />} />
-        <Route path="/questions/:id" element={<LazyQuestion />} />
-        {/* <Route path="*" element={<NotFoundPage />} /> */}
+        <Route path="/" element={<div />} />
       </Routes>
     </BrowserRouter>
   );
