a43ec2d02dca991f19f5223847d07e8bca950836
Dongming Shen <dongmingshen@icloud.com>
2024-02-01 04:58:10 +0000

    [PR]

--- src/screens/__tests__/HomePage.e2e.test.tsx
+++ src/screens/__tests__/HomePage.e2e.test.tsx
@@ -6,24 +6,22 @@ describe("HomePage E2E test", () => {
   let browser: Browser;
 
   beforeEach(async () => {
-    // browser = await puppeteer.launch({
-    //   ignoreHTTPSErrors: true,
-    //   args: [
-    //     "--no-sandbox",
-    //     "--disable-setuid-sandbox",
-    //     "--remote-debugging-port=18523",
-    //     "--remote-debugging-address=0.0.0.0",
-    //   ],
-    //   // executablePath: Config.browser.path,
-    //   headless: "new",
-    // });
-    // tmr test 1, run on local browser and see open a remote local browser.
-    // do in docker
-    // do reverse
-    browser = await puppeteer.connect({
-      browserWSEndpoint:
-        "ws://host.docker.internal:18523/devtools/page/409FCCEA23E319A5DD2BB37BA6E579B9",
+    browser = await puppeteer.launch({
+      ignoreHTTPSErrors: true,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--remote-debugging-port=18523",
+        "--remote-debugging-address=0.0.0.0",
+      ],
+      // executablePath: Config.browser.path,
+      headless: "new",
     });
+
+    // browser = await puppeteer.connect({
+    //   browserWSEndpoint:
+    //     "ws://host.docker.internal:18523/devtools/page/409FCCEA23E319A5DD2BB37BA6E579B9",
+    // });
   });
 
   afterEach(async () => {
@@ -33,10 +31,14 @@ describe("HomePage E2E test", () => {
   });
 
   test('should display "Welcome to React" text on page', async () => {
-    // Navigate to your application's home page
-    // Replace 'http://localhost:3000' with the URL of your application
     const page = await browser.newPage();
 
-    await page.goto("https://lol.com/", { waitUntil: "networkidle0" });
-  }, 50000);
+    // Navigate to your application's home page
+    // Replace 'http://localhost:3000' with the URL of your application
+    await page.goto("http://localhost:3000");
+
+    // Check if "Welcome to React" text is present on the page
+    const textContent = await page.evaluate(() => document.body.textContent);
+    expect(textContent).toContain("Welcome to React");
+  });
 });
